f0fd996c2c756edc94f926f01eb518a46da0f113
vcow <yvi@ambidexter.io>
2019-12-25 15:13:40 +0300

    crab Animations

---
+++ kolobrod-project/Assets/Scripts/AI/MonsterCrabController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace AI
+{
+	[DisallowMultipleComponent, RequireComponent(typeof(Animator))]
+	public class MonsterCrabController : MonoBehaviour
+	{
+		public void OnAttack()
+		{
+			
+		}
+	}
+}
