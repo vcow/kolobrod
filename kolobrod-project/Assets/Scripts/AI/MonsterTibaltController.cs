2dbef9113064e57bea942a01c9f74dc3ad933183
Yacow Vircowskiy <vcow@yandex.ru>
2020-01-05 15:25:53 +0300

    tibalt attack

--- kolobrod-project/Assets/Scripts/AI/MonsterTibaltController.cs
+++ kolobrod-project/Assets/Scripts/AI/MonsterTibaltController.cs
@@ -1,7 +1,14 @@
+using UnityEngine;
+using UnityEngine.Animations;
+
 namespace AI
 {
 	public class MonsterTibaltController : MonsterControllerBase
 	{
+#pragma warning disable 649
+		[SerializeField] private ParticleSystem _flame;
+#pragma warning restore 649
+
 		protected override void OnWalkChanged(bool walk)
 		{
 		}
@@ -12,6 +19,24 @@ namespace AI
 
 		protected override void UpdateDamagePercent(float damagePercent)
 		{
+		}
+
+		public void OnStartAttack()
+		{
+			_flame.Play(true);
+		}
+
+		public void OnFinishAttack()
+		{
+			_flame.Stop(true);
+		}
+
+		protected override void Die()
+		{
+			base.Die();
+
+			_flame.GetComponent<PositionConstraint>().constraintActive = false;
+			_flame.Stop(true);
 		}
 	}
 }
