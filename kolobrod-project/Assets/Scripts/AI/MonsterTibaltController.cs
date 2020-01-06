9e0eeb13181b9185342b70c8fbec1052c7fdea9b
Yacow Vircowskiy <vcow@yandex.ru>
2020-01-06 13:05:39 +0300

    tibalt death

--- kolobrod-project/Assets/Scripts/AI/MonsterTibaltController.cs
+++ kolobrod-project/Assets/Scripts/AI/MonsterTibaltController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Animations;
 
@@ -7,6 +8,8 @@ namespace AI
 	{
 #pragma warning disable 649
 		[SerializeField] private ParticleSystem _flame;
+		[SerializeField] private ParticleSystem _smoke;
+		[SerializeField] private ParticleSystem _fire;
 #pragma warning restore 649
 
 		protected override void OnWalkChanged(bool walk)
@@ -19,6 +22,17 @@ namespace AI
 
 		protected override void UpdateDamagePercent(float damagePercent)
 		{
+			if (damagePercent > 0.5f)
+			{
+				if (!_fire.isPlaying)
+				{
+					_fire.Play(true);
+					_smoke.Stop(true);
+				}
+
+				var e = _fire.emission;
+				e.rateOverTime = 5 * (damagePercent - 0.5f) / 0.5f;
+			}
 		}
 
 		public void OnStartAttack()
@@ -37,6 +51,35 @@ namespace AI
 
 			_flame.GetComponent<PositionConstraint>().constraintActive = false;
 			_flame.Stop(true);
+
+			if (!_smoke.isPlaying) _smoke.Play(true);
+
+			var e1 = _fire.emission;
+			var e2 = _smoke.emission;
+			DOTween.Sequence()
+				.Append(DOTween.To(() => 5, value => e1.rateOverTime = value, 100, 2).SetEase(Ease.InQuad))
+				.Join(DOTween.To(() => 3, value => e2.rateOverTime = value, 7, 2).SetEase(Ease.Linear))
+				.AppendCallback(() =>
+				{
+					foreach (var rigidBody in _rigidBodies)
+					{
+						rigidBody.simulated = false;
+					}
+				})
+				.Append(DOTween.To(() => Color.white, value =>
+				{
+					foreach (var meshInstance in MeshInstances)
+					{
+						meshInstance.color = value;
+					}
+				}, new Color(1, 1, 1, 0), 1).SetDelay(1))
+				.Append(DOTween.To(() => 100, value => e1.rateOverTime = value, 0, 1).SetEase(Ease.InQuad))
+				.Join(DOTween.To(() => 7, value => e2.rateOverTime = value, 0, 1).SetEase(Ease.Linear))
+				.OnComplete(() =>
+				{
+					_fire.Stop(true);
+					Destroy(gameObject, 5);
+				});
 		}
 	}
 }
